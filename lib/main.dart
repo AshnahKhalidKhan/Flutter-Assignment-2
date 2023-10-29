dd954d5cac0a3ad0c68783b82cfc29bc17f589bb
AshnahKhalidKhan <a.khan.22889@khi.iba.edu.pk>
2023-10-29 23:52:48 +0500

    Done done done

--- lib/main.dart
+++ lib/main.dart
@@ -10,53 +10,98 @@ void main()
   runApp(const MainApp());
 }
 
-class MainApp extends StatelessWidget 
-{
-  const MainApp({super.key});
+// class MainApp extends StatelessWidget 
+// {
+//   const MainApp({super.key});
+
+//   @override
+//   Widget build(BuildContext context) 
+//   {
+//     return MaterialApp
+//     (
+//       // home: Scaffold
+//       // (
+//       //   body: Center
+//       //   (
+//       //     child: Text('Hello World!'),
+//       //   ),
+//       // ),
+//       // home: LightThemeSignInScreen(),
+//       // home: LightThemeSignUpScreen(),
+//       // home: DarkThemeSignInScreen(),
+//       home: Scaffold
+//       (
+//         body: Column
+//         (
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           crossAxisAlignment: CrossAxisAlignment.center,
+//           children: 
+//           [
+//             ElevatedButton
+//             (
+//               onPressed: () 
+//               {
+//                 Navigator.push
+//                 (
+//                   context,
+//                   MaterialPageRoute(builder: (context) => LightThemeSignInScreen()),
+//                 );
+//               },
+//               child: Text('Light Theme'),
+//             ),
+//             ElevatedButton
+//             (
+//               onPressed: ()
+//               {
+//                 Navigator.push
+//                 (
+//                   context,
+//                   MaterialPageRoute(builder: (context) => DarkThemeSignInScreen()),
+//                 );
+//               },
+//               child: Text('Dark Theme'),
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+
+class MainApp extends StatelessWidget {
+  const MainApp({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) 
-  {
-    return MaterialApp
-    (
-      // home: Scaffold
-      // (
-      //   body: Center
-      //   (
-      //     child: Text('Hello World!'),
-      //   ),
-      // ),
-      // home: LightThemeSignInScreen(),
-      // home: LightThemeSignUpScreen(),
-      // home: DarkThemeSignInScreen(),
-      home: Scaffold
-      (
-        body: Column
-        (
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MainScreen(),
+      routes: {
+        '/lightThemeSignIn': (context) => LightThemeSignInScreen(),
+        '/darkThemeSignIn': (context) => DarkThemeSignInScreen(),
+      },
+    );
+  }
+}
+
+class MainScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
-          children: 
-          [
-            ElevatedButton
-            (
-              onPressed: () 
-              {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => LightThemeSignInScreen()),
-                );
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, '/lightThemeSignIn');
               },
               child: Text('Light Theme'),
             ),
-            ElevatedButton
-            (
-              onPressed: ()
-              {
-                Navigator.push
-                (
-                  context,
-                  MaterialPageRoute(builder: (context) => DarkThemeSignInScreen()),
-                );
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, '/darkThemeSignIn');
               },
               child: Text('Dark Theme'),
             ),
@@ -66,3 +111,4 @@ class MainApp extends StatelessWidget
     );
   }
 }
+
