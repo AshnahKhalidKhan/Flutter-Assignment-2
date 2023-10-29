85e0da0541d2ff0e4c6404f7d5efa779dd2f3d0e
AshnahKhalidKhan <a.khan.22889@khi.iba.edu.pk>
2023-10-29 06:56:44 +0500

    Scaffolding code

---
+++ lib/Widgets/LightThemeSignInScreen.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class LightThemeSignInScreen extends StatefulWidget 
+{
+  const LightThemeSignInScreen({super.key});
+
+  @override
+  State<LightThemeSignInScreen> createState() => _LightThemeSignInScreenState();
+}
+
+class _LightThemeSignInScreenState extends State<LightThemeSignInScreen> 
+{
+  @override
+  Widget build(BuildContext context) 
+  {
+    return Padding
+    (
+      padding: EdgeInsets.all(20.0),
+      child: Column
+      (
+        children: 
+        [
+          Text('Log In'),
+          TextFormField
+          (),
+        ]
+      ),
+    );
+  }
+}
