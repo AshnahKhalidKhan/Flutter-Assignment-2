a06a3fd3a03d1d3d2f8329b685153c34d922b946
AshnahKhalidKhan <a.khan.22889@khi.iba.edu.pk>
2023-10-29 16:02:17 +0500

    I guess it works...

--- lib/Widgets/LightThemeSignInScreen.dart
+++ lib/Widgets/LightThemeSignInScreen.dart
@@ -461,15 +461,10 @@ class _LightThemeSignInScreenState extends State<LightThemeSignInScreen>
 
       
       body: Column
-      (
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: 
-        [ 
-          // Container
-          // (
-          //   padding: EdgeInsets.only(left: 15.0, top: 10.0),
-            // height: 450,
-            //child: Expanded
+        (
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: 
+          [
             Expanded
             (
               child: Container
@@ -483,146 +478,144 @@ class _LightThemeSignInScreenState extends State<LightThemeSignInScreen>
                 ),
               ),
             ),
-          // ),
-          Expanded
-          (
-            child: Padding
+            Expanded
             (
-              // padding: const EdgeInsets.only(left: 30.0, right: 30.0, bottom: 30.0, top: 10.0),
-              padding: const EdgeInsets.all(30.0),
-              child: Column
-                (
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children:
-                  [
-                    const Text
-                    (
-                      "Log In",
-                      style: TextStyle
+              child: Padding
+              (
+                padding: const EdgeInsets.only(left: 30.0, right: 30.0, bottom: 30.0, top: 10.0),
+                child: Column
+                  (
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children:
+                    [
+                      const Text
                       (
-                        fontSize: 25,
-                        color: Color.fromARGB(255, 138, 129, 168),
-                        fontWeight: FontWeight.w500
-                      ),
-                    ),
-                    const SizedBox(height: 30.0),
-                    TextFormField
-                    (
-                      decoration: InputDecoration
-                      (
-                        border: OutlineInputBorder
-                        (
-                          borderRadius: BorderRadius.circular(10.0),
-                          borderSide: const BorderSide
-                          (
-                            color: Color.fromARGB(255, 138, 129, 168),
-                            width: 2.0,
-                            style:BorderStyle.solid
-                          )
-                        ),
-                      ),
-                      initialValue: 'Email',
-                      style: const TextStyle
-                      (
-                        color: Color.fromARGB(255, 138, 129, 168),
-                        fontWeight: FontWeight.w700
-                      ),
-                    ),
-                    const SizedBox(height: 20.0),
-                    TextFormField
-                    (
-                      decoration: InputDecoration
-                      (
-                        border: OutlineInputBorder
-                        (
-                          borderRadius: BorderRadius.circular(10.0),
-                          borderSide: const BorderSide
-                          (
-                            color: Color.fromARGB(255, 138, 129, 168),
-                            width: 2.0,
-                            style:BorderStyle.solid
-                          )
-                        ),
-                      ),
-                      initialValue: 'Password',
-                      style: const TextStyle
-                      (
-                        color: Color.fromARGB(255, 138, 129, 168),
-                        fontWeight: FontWeight.w700
-                      ),
-                    ),
-                    const SizedBox(height: 20.0),
-                    FilledButton.tonal
-                    (
-                      onPressed: () {},
-                      child: const Text
-                      (
-                        'Sign In',
+                        "Log In",
                         style: TextStyle
                         (
-                          color: Colors.white,
-                          fontWeight: FontWeight.w400
+                          fontSize: 25,
+                          color: Color.fromARGB(255, 138, 129, 168),
+                          fontWeight: FontWeight.w500
                         ),
                       ),
-                      style: const ButtonStyle
+                      const SizedBox(height: 30.0),
+                      TextFormField
                       (
-                        backgroundColor: MaterialStatePropertyAll(Color.fromARGB(255, 146, 122, 244)),
-                        minimumSize: MaterialStatePropertyAll(Size.fromHeight(50)),
-                        maximumSize: MaterialStatePropertyAll(Size.infinite),
-                      ),
-                    ),
-                    const SizedBox(height: 20.0),
-                    Row
-                    (
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: 
-                      [
-                        const Text
+                        decoration: InputDecoration
                         (
-                          "Don't have an account?",
-                          style: TextStyle
+                          border: OutlineInputBorder
                           (
-                            fontSize: 15,
-                          ),
-                        ),
-                        const SizedBox(width: 2.0),
-                        GestureDetector
-                        (
-                          child: const Text
-                          (
-                            "Sign Up",
-                            style: TextStyle
+                            borderRadius: BorderRadius.circular(10.0),
+                            borderSide: const BorderSide
                             (
-                              fontSize: 15,
-                              color: Color.fromARGB(255, 132, 115, 189),
-                              fontWeight: FontWeight.w700
-                            ),
+                              color: Color.fromARGB(255, 138, 129, 168),
+                              width: 2.0,
+                              style:BorderStyle.solid
+                            )
                           ),
-                          onTap: () {},
                         ),
-                      ],
-                    ),
-                    const SizedBox(height: 20.0),
-                    GestureDetector
-                    (
-                      child: const Text
-                      (
-                        "Forgot Password?",
-                        style: TextStyle
+                        initialValue: 'Email',
+                        style: const TextStyle
                         (
-                          fontSize: 15,
-                          color: Color.fromARGB(255, 132, 115, 189),
+                          color: Color.fromARGB(255, 138, 129, 168),
                           fontWeight: FontWeight.w700
                         ),
                       ),
-                      onTap: () {},
-                    ),
-                  ],
-                ),
-              )
-          ),    
-        ],
-      ),
+                      const SizedBox(height: 20.0),
+                      TextFormField
+                      (
+                        decoration: InputDecoration
+                        (
+                          border: OutlineInputBorder
+                          (
+                            borderRadius: BorderRadius.circular(10.0),
+                            borderSide: const BorderSide
+                            (
+                              color: Color.fromARGB(255, 138, 129, 168),
+                              width: 2.0,
+                              style:BorderStyle.solid
+                            )
+                          ),
+                        ),
+                        initialValue: 'Password',
+                        style: const TextStyle
+                        (
+                          color: Color.fromARGB(255, 138, 129, 168),
+                          fontWeight: FontWeight.w700
+                        ),
+                      ),
+                      const SizedBox(height: 20.0),
+                      FilledButton.tonal
+                      (
+                        onPressed: () {},
+                        child: const Text
+                        (
+                          'Sign In',
+                          style: TextStyle
+                          (
+                            color: Colors.white,
+                            fontWeight: FontWeight.w400
+                          ),
+                        ),
+                        style: const ButtonStyle
+                        (
+                          backgroundColor: MaterialStatePropertyAll(Color.fromARGB(255, 146, 122, 244)),
+                          minimumSize: MaterialStatePropertyAll(Size.fromHeight(50)),
+                          maximumSize: MaterialStatePropertyAll(Size.infinite),
+                        ),
+                      ),
+                      const SizedBox(height: 20.0),
+                      Row
+                      (
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: 
+                        [
+                          const Text
+                          (
+                            "Don't have an account?",
+                            style: TextStyle
+                            (
+                              fontSize: 15,
+                            ),
+                          ),
+                          const SizedBox(width: 2.0),
+                          GestureDetector
+                          (
+                            child: const Text
+                            (
+                              "Sign Up",
+                              style: TextStyle
+                              (
+                                fontSize: 15,
+                                color: Color.fromARGB(255, 132, 115, 189),
+                                fontWeight: FontWeight.w700
+                              ),
+                            ),
+                            onTap: () {},
+                          ),
+                        ],
+                      ),
+                      const SizedBox(height: 20.0),
+                      GestureDetector
+                      (
+                        child: const Text
+                        (
+                          "Forgot Password?",
+                          style: TextStyle
+                          (
+                            fontSize: 15,
+                            color: Color.fromARGB(255, 132, 115, 189),
+                            fontWeight: FontWeight.w700
+                          ),
+                        ),
+                        onTap: () {},
+                      ),
+                    ],
+                  ),
+                )
+            ),    
+          ],
+        ),
     );
   }
 }
